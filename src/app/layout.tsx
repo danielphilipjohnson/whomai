7fa9fa62f34124c876f2a45800fac914aea4bb76
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-12 21:00:05 +0100

    Add animations and hover effects in globals.css; restructure layout in RootLayout to include CyberpunkBackground, Topbar, and Footer components; replace main content in Home with CyberpunkDesktop component for enhanced UI experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Topbar } from "@/components/layout/Topbar";
+import CyberpunkBackground from "@/components/layout/DesktopBackground";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="h-screen w-full flex flex-col bg-gray-900 overflow-hidden relative">
+          <CyberpunkBackground />
+          <Topbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
