3444b5ed4f2e378ea3f3f558b82b2293bd21f314
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-10-23 09:54:39 +0100

    hardcode domain as a test

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -41,7 +41,7 @@ export const ThemeSwitcher = ({ onSelect }: ThemeSwitcherProps) => {
             >
               <div className="relative h-24 w-full sm:h-20">
                 <Image
-                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/${image}`}
+                  src={`https://danielphilipjohnson.github.io/whomai${image}`}
                   alt={`${name} background`}
                   sizes="(min-width: 640px) 120px, 200px"
                   className="object-cover transition duration-200 group-hover:scale-105"
